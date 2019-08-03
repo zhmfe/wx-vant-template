39797f6d93ec8cf4243a826be4cb6e17aef607da
zhanghm <zhanghuimin@baiyaodajiankang.com>
2019-08-03 14:12:17 +0800

    feat: update config

--- template/config/index.js
+++ template/config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/': {
+        target: '',
+        changeOrigin: true,
+        pathRewrite: {
+            '/': '/'
+        }
+    }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -51,7 +59,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: process.env.ROUTER_BASE,
 
     /**
      * Source Maps
